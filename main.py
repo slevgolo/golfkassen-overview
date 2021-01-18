c5615c9f8fbd4de70a7bbfe20e4dc5209fa01410
slevink94 <marcelr94@gmx.de>
2021-01-18 21:05:16 +0100

    add files and first tests

---
+++ main.py
@@ -0,0 +1,47 @@
+import sys
+import smtplib
+import ssl
+import pandas as pd
+
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from datetime import datetime
+
+
+# get arguments
+sender_email = sys.argv[1]
+sender_password = sys.argv[2]
+receiver_emails = [addr for addr in sys.argv[3:]]
+
+# set port for gmx.net
+port = 465
+
+# set defaul ssl context
+context = ssl.create_default_context()
+
+# create message
+timestamp = datetime.now().strftime("%d-%b-%Y %H:%M:%S")
+msg = MIMEMultipart()
+msg['From'] = sender_email
+msg['To'] = sender_email
+msg['Subject'] = 'Aktueller Stand Golfkasse ' + timestamp
+
+# email text
+df = pd.read_csv('golfkasse.csv')
+dic = {'Marcel':0, 'Felix':0, 'Flo':0}
+for key in dic.keys():
+    dic[key] = df.iloc[(df['name'] == key).values,2].sum()
+
+body = 'Guten Morgen!\nAktuell verteilen sich die {:.2f}€ in der Golfkasse wie folgt:\n'.format(round(sum(dic.values()), 2))
+a = ''
+for key in dic.keys():
+    a += key + ': ' + '{:.2f}'.format(dic[key]) + '€\n'
+body += a + '\n\nMit freundlichen Grüßen\nGolfKassenOverview'
+
+body = 'testmail'
+msg.attach(MIMEText(body, 'plain'))
+
+# send mail
+with smtplib.SMTP_SSL('mail.gmx.net', port, context=context) as server:
+    server.login(sender_email, sender_password)
+    server.sendmail(sender_email, receiver_emails, msg.as_string())
