8d8838eb5b9775ff47afbffcfd6332d5fefd71ad
slevink94 <marcelr94@gmx.de>
2021-01-25 17:52:42 +0100

    remove pandas import and add some print statements

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+print('Start importing modules...')
 import sys
 import smtplib
 import ssl
@@ -6,9 +7,7 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from datetime import datetime
 
-import pandas as pd
-
-
+print('Read arguments...')
 # get arguments
 sender_email = sys.argv[1]
 sender_password = sys.argv[2]
@@ -20,6 +19,7 @@ port = 465
 # set defaul ssl context
 context = ssl.create_default_context()
 
+print('Create message')
 # create message
 datestamp = datetime.now().strftime("%d-%b-%Y")
 msg = MIMEMultipart()
@@ -27,23 +27,31 @@ msg['From'] = sender_email
 msg['To'] = sender_email
 msg['Subject'] = datestamp + ': Aktueller Stand Golfkasse'
 
+print('Get E-Mail text')
 # email text
-df = pd.read_csv('golfkasse.csv')
+with open('golfkasse.csv', 'r') as f:
+    data = f.read().splitlines()
+
 dic = {'Marcel':0, 'Felix':0, 'Flo':0}
-for key in dic.keys():
-    dic[key] = df.iloc[(df['name'] == key).values,2].sum()
+for line in data[1:]:
+    split = line.split(',')
+    dic[split[1]] += float(split[2])
 
 body = 'Guten Morgen!\n\nAktuell verteilen sich die'
 body += ' {:.2f}€ in der Golfkasse wie folgt:\n'.format(
     round(sum(dic.values()), 2))
 
 a = ''
-for key in dic.keys():
+for key in sorted(dic.keys()):
     a += key + ': ' + '{:.2f}'.format(dic[key]) + '€\n'
 body += a + '\nMit freundlichen Grüßen\nGolfKassenOverview'
 
 msg.attach(MIMEText(body, 'plain'))
 
+print('Send mail to:')
+for receiver_email in receiver_emails:
+    print(receiver_email)
+    
 # send mail
 with smtplib.SMTP_SSL('mail.gmx.net', port, context=context) as server:
     server.login(sender_email, sender_password)
